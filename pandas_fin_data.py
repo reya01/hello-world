49afa1561bc5be950e52ed1498d70e9c606f421c
reya01 <86735675+reya01@users.noreply.github.com>
2021-09-04 15:32:46 -0400

    Update pandas_fin_data.py

--- pandas_fin_data.py
+++ pandas_fin_data.py
@@ -1,3 +1,5 @@
+#pip install pandas-datareader
+
 from datetime import datetime
 
 import pandas_datareader.data as web
@@ -9,4 +11,9 @@ mortgage_30_2019 = web.DataReader('MORTGAGE30US', 'fred', start, end)
 
 sap_data = web.DataReader('SP500', 'fred', start, end)
 
+inflation_2019 = web.DataReader('CPALTT01USM657N', 'fred', start, end)
+
 print(sap_data)
+print(inflation_2019)
+
+inflation_2019.to_csv('inflation_2019.csv')
